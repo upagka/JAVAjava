0aee62fee56fb05c1cb98967ab7b1974f65e594d
upagka <upagka@yandex.com>
2021-09-01 02:09:06 +0600

    01.09 - 2:08 HW2

--- src/main/java/Lesson2/Homework2.java
+++ src/main/java/Lesson2/Homework2.java
@@ -3,11 +3,70 @@ package Lesson2;
 public class Homework2 {
     static public void main(String[] args) {
 
- //       ex1();
+//        ex1();
+        System.out.println();
+//        ex2();
+        System.out.println();
+//        ex3();
+        System.out.println();
+//        ex4();
+        System.out.println();
+//        exercise5
+//        int[] bal = {2, 13, 2, 1, 2, 2, 10, 1, 4};
+//        System.out.println(ex5(bal));
 
-//       ex2();
+    }
 
+    public static boolean ex5(int[] balance) {
 
+        int leftSide = balance[0];
+        int rightSide = balance[balance.length - 1];
+        int rightPosition = balance.length - 2;
+        int leftPosition = 1;
+
+        do {
+            if (leftSide > rightSide) {
+                rightSide += balance[rightPosition--];
+            }
+            else {
+                leftSide += balance[leftPosition++];
+            }
+        } while (leftPosition != rightPosition + 1);
+
+        if (leftSide == rightSide) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    private static void ex4() {
+        int[] minMax = {6, 15, 3, 2, 11, -4, 5, 2, 78, 8, 9, 1};
+        int minDigit = minMax[0];
+        int maxDigit = minMax[0];
+
+        for (int element : minMax) {
+            if (minDigit > element) {
+                minDigit = element;
+            }
+            if (maxDigit < element) {
+               maxDigit = element;
+            }
+        }
+
+        System.out.println("Min: " + minDigit + "; " + "Max: " + maxDigit);
+    }
+
+    private static void ex3() {
+        int[] lessSix = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+
+        for (int element : lessSix) {
+            if (element < 6) {
+                element *= 2;
+            }
+            System.out.print(element + " ");
+        }
     }
 
     private static void ex2() {
