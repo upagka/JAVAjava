faab7a85ed92098888d390609f353a0fe3aabc14
upagka <upagka@yandex.com>
2021-09-08 21:51:31 +0600

    CW 08.09.21

--- src/main/java/Lesson4/TicTacToe.java
+++ src/main/java/Lesson4/TicTacToe.java
@@ -1,80 +1,147 @@
 package Lesson4;
 
-
-
 import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToe {
-
-    private static final char DOT_HUMAN = 'x';
+    private static final char DOT_HUMAN = 'X';
     private static final char DOT_AI = '0';
     private static final char DOT_EMPTY = '.';
-
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final Random RANDOM = new Random();
-    public static char[][] field;
+
+    private static char[][] field;
 
     private static int fieldSizeX;
     private static int fieldSizeY;
     private static int scoreHuman;
-    private static int scoreAi;
+    private static int scoreAI;
+    private static int roundCounter = 1;
 
     public static void main(String[] args) {
-
-        initField(3, 3);
-        printField();
-        humanTurn();
-        printField();
-        aiTurn();
-        printField();
+        play();
     }
 
-    private static void humanTurn() {
-        int x, y;
-
-    do {
-        System.out.print("Please enter coodinates of ypur turn x & y spit by whitespace >>>>>");
-        x = SCANNER.nextInt() - 1;
-        y = SCANNER.nextInt() - 1;
-    } while (!isCellValid(y, x) || !isCellEmpty(y, x));
-        field[y][x] = DOT_HUMAN;
-    }
-
-    private static void aiTurn() {
-        int x, y;
-
-    do {
-        x = RANDOM.nextInt(fieldSizeX);
-        y = RANDOM.nextInt(fieldSizeY);
-
-        } while (!isCellEmpty(y, x));
-        field[y][x] = DOT_AI;
-    }
-
-
-    private static boolean isCellValid(int y, int x) {
-        return x >= 0 && y >= 0 && x < fieldSizeX && y < fieldSizeY;
-    }
-    private static boolean isCellEmpty(int y, int x) {
-        return field[y][x] == DOT_EMPTY;
-    }
-    private static void initField(int sizeX, int sizeY) {
-        fieldSizeY = sizeY;
-        fieldSizeX = sizeX;
-        field = new char[sizeY][sizeX];
-        for (int y = 0; y < sizeY; y++) {
-            for (int x = 0; x < sizeX; x++) {
-                field[y][x] = DOT_EMPTY;
+    private static void play() {
+        while (true) {
+            playRound();
+            System.out.printf("SCORE: HUMAN    AI\n" +
+                    "         %d      %d\n", scoreHuman, scoreAI);
+            System.out.print("Wanna play again? Y or N >>>> ");
+            if (!SCANNER.next().toLowerCase().equals("y")) {
+                System.out.println("Good bye!");
+                break;
             }
         }
 
     }
 
+    private static void playRound() {
+        System.out.printf("ROUND %d\n", roundCounter++);
+        initField(3, 3);
+        printField();
+
+        while (true) {
+            humanTurn();
+            printField();
+            if (checkGame(DOT_HUMAN)) break;
+            aiTurn();
+            printField();
+            if (checkGame(DOT_AI)) break;
+        }
+    }
+
+    private static boolean checkGame(char dot) {
+        if (checkDraw()) return true;
+        if (checkWin(dot)) {
+            if (dot == DOT_HUMAN) {
+                System.out.println("HUMAN WINS!!!");
+                scoreHuman++;
+            } else {
+                System.out.println("AI WINS!!!");
+                scoreAI++;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private static void humanTurn() {
+        int x, y;
+
+        do {
+            System.out.print("Please enter coordinates of your turn x & y split by whitespace >>>> ");
+            x = SCANNER.nextInt() - 1;
+            y = SCANNER.nextInt() - 1;
+        } while (!isCellValid(y, x) || !isCellEmpty(y, x));
+
+        field[y][x] = DOT_HUMAN;
+    }
+
+    //***** Доработать искусственный интеллект, чтобы он мог блокировать ходы игрока.
+    private static void aiTurn() {
+        int x, y;
+        do {
+            x = RANDOM.nextInt(fieldSizeX);
+            y = RANDOM.nextInt(fieldSizeY);
+        } while (!isCellEmpty(y, x));
+        field[y][x] = DOT_AI;
+    }
+
+    private static boolean checkDraw() {
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                if (isCellEmpty(y, x)) return false;
+            }
+        }
+        System.out.println("DRAW!!!");
+        return true;
+    }
+
+    //Переделать проверку победы, чтобы она не была реализована просто набором условий, например, с использованием циклов.
+    //*Попробовать переписать логику проверки победы, чтобы она работала для поля 5х5 и количества фишек 4. Очень желательно не делать это просто набором условий для каждой из возможных ситуаций;
+    //*** Попробовать сделать универсальную проверку, для произвольной длины последовательности
+    // и произвольного размера поля
+    private static boolean checkWin(char dot) {
+        //hor
+        if (field[0][0] == dot && field[0][1] == dot && field[0][2] == dot) return true;
+        if (field[1][0] == dot && field[1][1] == dot && field[1][2] == dot) return true;
+        if (field[2][0] == dot && field[2][1] == dot && field[2][2] == dot) return true;
+
+        //ver
+        if (field[0][0] == dot && field[1][0] == dot && field[2][0] == dot) return true;
+        if (field[0][1] == dot && field[1][1] == dot && field[2][1] == dot) return true;
+        if (field[0][2] == dot && field[1][2] == dot && field[2][2] == dot) return true;
+
+        //diag
+        if (field[0][0] == dot && field[1][1] == dot && field[2][2] == dot) return true;
+        if (field[0][2] == dot && field[1][1] == dot && field[2][0] == dot) return true;
+        return false;
+    }
+
+    private static boolean isCellValid(int y, int x) {
+        return x >= 0 && y >= 0 && x < fieldSizeX && y < fieldSizeY;
+    }
+
+    private static boolean isCellEmpty(int y, int x) {
+        return field[y][x] == DOT_EMPTY;
+    }
+
+    private static void initField(int sizeX, int sizeY) {
+        fieldSizeY = sizeY;
+        fieldSizeX = sizeX;
+        field = new char[sizeY][sizeX];
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                field[y][x] = DOT_EMPTY;
+            }
+        }
+    }
+
     static void printField() {
-        System.out.print(" ");
+        System.out.print("+");
         for (int i = 0; i < fieldSizeX * 2 + 1; i++) {
-            System.out.print(i % 2 == 0 ? "_" : i / 2 + 1);
+            System.out.print(i % 2 == 0 ? "-" : i / 2 + 1);
         }
         System.out.println();
         for (int i = 0; i < fieldSizeY; i++) {
@@ -84,16 +151,10 @@ public class TicTacToe {
             }
             System.out.println();
         }
-        System.out.print(" ");
-        for (int i = 0; i < fieldSizeX * 2 + 1; i++) {
-            System.out.print('\u0305');
+        for (int i = 0; i <= fieldSizeX * 2 + 1; i++) {
+            System.out.print("-");
         }
         System.out.println();
-
-
-
-
-
 //        for (int y = 0; y < fieldSizeY; y++) {
 //            for (int x = 0; x < fieldSizeX; x++) {
 //                System.out.print(field[y][x] + "  ");
@@ -101,5 +162,4 @@ public class TicTacToe {
 //            System.out.println();
 //        }
     }
-
 }
